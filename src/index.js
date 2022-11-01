047a4678416f8147675806da14de5652f8d7fcf7
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-10-31 20:02:25 -0500

    Create API fetch function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import css from './style.css';
+
+function test() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=34.052235&lon=-118.243683&appid=3bdeb7609c3eff9d1997a47ba5ce1830', { mode:'cors'})
+        .then(function(response) {
+            console.log(response.json());
+        })
+        .catch(function(err){
+            console.log('error')
+        })
+}
+
+test();
